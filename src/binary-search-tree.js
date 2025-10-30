d1400c2f42f39e0d38f3e9c01022a281aa9de47d
Yegor000 <122170879+Yegor000@users.noreply.github.com>
2025-10-30 18:42:42 +0300

    feat: implement BinarySearchTree class

--- src/binary-search-tree.js
+++ src/binary-search-tree.js
@@ -1,44 +1,183 @@
 const { NotImplementedError } = require('../lib/errors');
-// const { Node } = require('../extensions/list-tree.js');
+const { Node } = require('../extensions/list-tree.js');
 
 /**
 * Implement simple binary search tree according to task description
 * using Node from extensions
 */
 class BinarySearchTree {
+  constructor() {
+    this.r = null;
+  }
+
   root() {
-    // Remove line below and write your code here
-    throw new NotImplementedError('Not implemented');
+    return this.r;
   }
 
-  add(/* data */) {
-    // Remove line below and write your code here
-    throw new NotImplementedError('Not implemented');
+  add(data) {
+    if (this.r === null) {
+      this.r = new Node(data);
+      return;
+    }
+
+    let currentNode = this.r;
+
+    while (true) {
+      if (data > currentNode.data) {
+        if (currentNode.right === null) {
+          currentNode.right = new Node(data);
+          return;
+        }
+        else {
+          currentNode = currentNode.right;
+        }
+      }
+      else {
+        if (currentNode.left === null) {
+          currentNode.left = new Node(data);
+          return;
+        }
+        else {
+          currentNode = currentNode.left;
+        }
+      }
+    }
   }
 
-  find(/* data */) {
-    // Remove line below and write your code here
-    throw new NotImplementedError('Not implemented');
+  find(data) {
+    let currentNode = this.r;
+
+    while (true) {
+      if (data > currentNode.data) {
+        if (currentNode.right === null) {
+          return null;
+        }
+        else {
+          currentNode = currentNode.right;
+        }
+      }
+      else if (data < currentNode.data) {
+        if (currentNode.left === null) {
+          return null;
+        }
+        else {
+          currentNode = currentNode.left;
+        }
+      }
+      else {
+        return currentNode;
+      }
+    }
   }
 
-  has(/* data */) {
-    // Remove line below and write your code here
-    throw new NotImplementedError('Not implemented');
+  has(data) {
+    let currentNode = this.r;
+
+    while (true) {
+      if (data > currentNode.data) {
+        if (currentNode.right === null) {
+          return false;
+        }
+        else {
+          currentNode = currentNode.right;
+        }
+      }
+      else if (data < currentNode.data) {
+        if (currentNode.left === null) {
+          return false;
+        }
+        else {
+          currentNode = currentNode.left;
+        }
+      }
+      else {
+        return true;
+      }
+    }
   }
 
-  remove(/* data */) {
-    // Remove line below and write your code here
-    throw new NotImplementedError('Not implemented');
+  remove(data) {
+    let currentNode = this.r;
+    let leftSide, rightSide;
+
+    while (true) {
+      if (data > currentNode.data) {
+        if (currentNode.right === null) {
+          return null;
+        }
+        else if (currentNode.right.data === data) {
+          rightSide = currentNode.right.right;
+          leftSide = currentNode.right.left;
+          currentNode.right = this.connect(leftSide, rightSide);
+          return;
+        }
+        else {
+          currentNode = currentNode.right;
+        }
+      }
+      else if (data < currentNode.data) {
+        if (currentNode.left === null) {
+          return null;
+        }
+        else if (currentNode.left.data === data) {
+          rightSide = currentNode.left.right;
+          leftSide = currentNode.left.left;
+          currentNode.left = this.connect(leftSide, rightSide);
+          return;
+        }
+        else {
+          currentNode = currentNode.left;
+        }
+      }
+      else {
+        this.r = this.connect(this.r.left, this.r.right);
+        return;
+      }
+    }
+  }
+
+  connect(leftNode, rigtNode) {
+    if(!leftNode || !rigtNode) {
+      return (leftNode || rigtNode)
+    }
+
+    let currentNode = rigtNode;
+
+    while (true) {
+      if (currentNode.left === null) {
+        currentNode.left = leftNode;
+        return rigtNode;
+      }
+      else {
+        currentNode = currentNode.left;
+      }
+    }
   }
 
   min() {
-    // Remove line below and write your code here
-    throw new NotImplementedError('Not implemented');
+    let currentNode = this.r;
+
+    while (true) {
+      if (currentNode.left === null) {
+        return currentNode.data;
+      }
+      else {
+        currentNode = currentNode.left;
+      }
+    }
   }
 
   max() {
-    // Remove line below and write your code here
-    throw new NotImplementedError('Not implemented');
+    let currentNode = this.r;
+
+    while (true) {
+      if (currentNode.right === null) {
+        return currentNode.data;
+      }
+      else {
+        currentNode = currentNode.right;
+      }
+    }
   }
 }
 
